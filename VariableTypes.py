c99136a49bc6d6d1cdfa4ebdfe13766881b10e3b
Avijit Das <avijitavi338895@gmail.com>
2024-04-01 00:41:14 +0600

    Types of variables

---
+++ VariableTypes.py
@@ -0,0 +1,21 @@
+name1 = "Avijit"
+name2 = "Das"
+age = 23
+salary = 25.50
+x = False
+y = None
+
+
+print("My name is: ",name1,name2)
+print("My age is: ",age)
+print("My salary is: ",salary)
+print("Married? ",x)
+
+print("\n")
+
+print("Name variable type: ",type(name1))
+print("Age variable type: ",type(age))
+print("Salary variable type: ",type(salary))
+print("X variable type: ",type(x))
+print("Y variable type: ",type(y))
+
